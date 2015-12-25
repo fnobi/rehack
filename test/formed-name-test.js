0773592ac1fedaf9d0baba5a76fb0d4eb1fed203
fujisawa-shin <fujisawa-shin@kayac.com>
2015-12-25 21:40:16 +0900

    test調整・large chain caseに対応

--- test/formed-name-test.js
+++ test/formed-name-test.js
@@ -3,83 +3,51 @@ var expect = require('chai').expect;
 var FormedName = require(__dirname + '/../lib/FormedName');
 
 describe('FormedName', function () {
-    it('chain case to camel case', function () {
+    it('parse chain case', function () {
         var name = new FormedName('hoge-moge');
+        expect(name.toChainCase()).to.be.equal('hoge-moge');
+        expect(name.toSnakeCase()).to.be.equal('hoge_moge');
         expect(name.toCamelCase()).to.be.equal('hogeMoge');
+        expect(name.toClassName()).to.be.equal('HogeMoge');
     });
-
-    it('chain case to class name', function () {
-        var name = new FormedName('hoge-moge');
+    
+    it('parse snake case', function () {
+        var name = new FormedName('hoge_moge');
+        expect(name.toChainCase()).to.be.equal('hoge-moge');
+        expect(name.toSnakeCase()).to.be.equal('hoge_moge');
+        expect(name.toCamelCase()).to.be.equal('hogeMoge');
+        expect(name.toClassName()).to.be.equal('HogeMoge');
+    });
+    
+    it('parse camel case', function () {
+        var name = new FormedName('hogeMoge');
+        expect(name.toChainCase()).to.be.equal('hoge-moge');
+        expect(name.toSnakeCase()).to.be.equal('hoge_moge');
+        expect(name.toCamelCase()).to.be.equal('hogeMoge');
         expect(name.toClassName()).to.be.equal('HogeMoge');
     });
 
-    it('chain case to snake case', function () {
-        var name = new FormedName('hoge-moge');
-        expect(name.toSnakeCase()).to.be.equal('hoge_moge');
-    });
-
-    it('chain case to chain case', function () {
-        var name = new FormedName('hoge-moge');
+    it('parse class name', function () {
+        var name = new FormedName('HogeMoge');
         expect(name.toChainCase()).to.be.equal('hoge-moge');
-    });
-
-    it('snake case to camel case', function () {
-        var name = new FormedName('hoge_moge');
+        expect(name.toSnakeCase()).to.be.equal('hoge_moge');
         expect(name.toCamelCase()).to.be.equal('hogeMoge');
-    });
-
-    it('snake case to class name', function () {
-        var name = new FormedName('hoge_moge');
         expect(name.toClassName()).to.be.equal('HogeMoge');
     });
 
-    it('snake case to snake case', function () {
-        var name = new FormedName('hoge_moge');
-        expect(name.toSnakeCase()).to.be.equal('hoge_moge');
-    });
-
-    it('snake case to chain case', function () {
-        var name = new FormedName('hoge_moge');
+    it('parse large chain case', function () {
+        var name = new FormedName('HOGE-MOGE');
         expect(name.toChainCase()).to.be.equal('hoge-moge');
-    });
-
-    it('camel case to camel case', function () {
-        var name = new FormedName('hogeMoge');
+        expect(name.toSnakeCase()).to.be.equal('hoge_moge');
         expect(name.toCamelCase()).to.be.equal('hogeMoge');
-    });
-
-    it('camel case to class name', function () {
-        var name = new FormedName('hogeMoge');
         expect(name.toClassName()).to.be.equal('HogeMoge');
     });
 
-    it('camel case to snake case', function () {
-        var name = new FormedName('hogeMoge');
-        expect(name.toSnakeCase()).to.be.equal('hoge_moge');
-    });
-
-    it('camel case to chain case', function () {
-        var name = new FormedName('hogeMoge');
-        expect(name.toChainCase()).to.be.equal('hoge-moge');
-    });
-
-    it('class name to camel case', function () {
-        var name = new FormedName('HogeMoge');
-        expect(name.toCamelCase()).to.be.equal('hogeMoge');
-    });
-
-    it('class name to class name', function () {
-        var name = new FormedName('HogeMoge');
-        expect(name.toClassName()).to.be.equal('HogeMoge');
-    });
-
-    it('class name to snake case', function () {
-        var name = new FormedName('HogeMoge');
-        expect(name.toSnakeCase()).to.be.equal('hoge_moge');
-    });
-
-    it('class name to chain case', function () {
-        var name = new FormedName('HogeMoge');
-        expect(name.toChainCase()).to.be.equal('hoge-moge');
+    it('parse single word', function () {
+        var name = new FormedName('hoge');
+        expect(name.toChainCase()).to.be.equal('hoge');
+        expect(name.toSnakeCase()).to.be.equal('hoge');
+        expect(name.toCamelCase()).to.be.equal('hoge');
+        expect(name.toClassName()).to.be.equal('Hoge');
     });
 });
